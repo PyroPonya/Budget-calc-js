61fb8a6955e0c7da223fad75a5abb12e463d5416
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2021-07-02 18:35:22 +0300

    main task

--- main.js
+++ main.js
@@ -7,12 +7,21 @@
  * - period
  */
 
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period = 0;
+let money = 100;
+let income = "freelance";
+let addExpenses = "интернет, такси, коммуналка";
+let deposit = false;
+let mission = 700;
+let period = 5;
 
-alert("pop!");
-console.log("test text");
+console.log(`typeof money => ${typeof money}`);
+console.log(`typeof money => ${typeof income}`);
+console.log(`typeof money => ${typeof deposit}`);
+console.log(`str.len => ${addExpenses.length}`);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLowerCase().split(""));
+
+let budgetDay = Math.round((money / 30) * 10) / 10;
+
+console.log(`budget per day => ${budgetDay}`);
