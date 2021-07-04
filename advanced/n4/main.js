ba0efc1206fe6baedd6b065631ac360f983aae0a
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2021-07-04 15:11:11 +0300

    task-04-advanced-fix-1

--- advanced/n4/main.js
+++ advanced/n4/main.js
@@ -4,17 +4,16 @@
 — В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
 — Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
  */
-const stringTheGreatest = prompt(
-  "Введите произвольную строку",
-  "         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis officia, architecto possimus deserunt excepturi nobis modi assumenda! Et exercitationem consequuntur natus perferendis eos! Vitae voluptate ullam, similique in officiis voluptatum aperiam, cupiditate, placeat doloribus error repellat! Soluta vitae laboriosam delectus esse quaerat, culpa sit possimus eum incidunt provident, eos sequi!         "
-);
+const stringTheGreatest = 
+  "         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis officia, architecto possimus deserunt excepturi nobis modi assumenda! Et exercitationem consequuntur natus perferendis eos! Vitae voluptate ullam, similique in officiis voluptatum aperiam, cupiditate, placeat doloribus error repellat! Soluta vitae laboriosam delectus esse quaerat, culpa sit possimus eum incidunt provident, eos sequi!         ";
+const stringTheSmallest = '               Lorem ipsum.                 ';
 const stringTrimmer = (el) => {
   if (typeof el !== "string") {
     return new Error() + "! Выведи и введи нормально!";
   }
   console.log(`Входная строка:
-  ${el}`);
-  if (el.length > 30) {
+  [${el}]`);
+  if (el.trim().length > 30) {
     return el.trim().slice(0, 30) + "(...)";
   } else {
     return el.trim();
@@ -22,4 +21,6 @@ const stringTrimmer = (el) => {
 };
 
 console.log(`Ваша строка после обработки:
-${stringTrimmer(stringTheGreatest)}`);
+[${stringTrimmer(stringTheGreatest)}]`);
+console.log(`Ваша строка после обработки:
+[${stringTrimmer(stringTheSmallest)}]`);
