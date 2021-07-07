a2ea1feb32919c0b3b6c179e870682b8c5a6316c
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2021-07-08 02:06:55 +0300

    lesson-07-advanced-fix-1

--- advanced/n7/main.js
+++ advanced/n7/main.js
@@ -12,13 +12,13 @@
 "use strict";
 
 const days = [
-  "Воскресенье".italics(),
+  "Воскресенье",
   "Понедельник",
   "Вторник",
   "Среда",
   "Четверг",
   "Пятница",
-  "Суббота".italics(),
+  "Суббота",
 ];
 const date = new Date();
 const dayId = date.getDay();
@@ -28,7 +28,47 @@ for (let i = 0; i < days.length; i++) {
     document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[
       i
     ].bold()}</span>`;
+  } else if (i === 0 || i === 6) {
+    document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[
+      i
+    ].italics()}</span>`;
   } else {
     document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+  }
+}
+document.body.innerHTML += "==========================";
+
+for (let i = 0; i < days.length; i++) {
+  switch (i) {
+    case dayId:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[
+        i
+      ].bold()}</span>`;
+      break;
+    case 0:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[
+        i
+      ].italics()}</span>`;
+      break;
+    case 1:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+      break;
+    case 2:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+      break;
+    case 3:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+      break;
+    case 4:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+      break;
+    case 5:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[i]}</span>`;
+      break;
+    case 6:
+      document.body.innerHTML += `<span style="font-size: 40px; display:flex">${days[
+        i
+      ].italics()}</span>`;
+      break;
   }
 }
